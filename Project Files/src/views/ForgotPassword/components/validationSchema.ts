adec82a6a2e1b29a801013d9d95c63302b5f3a16
MOHAMMED FARHAN <farhanmd789@gmail.com>
2026-02-18 15:37:33 +0530

    Delete Project Files directory

--- Project Files/src/views/ForgotPassword/components/validationSchema.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-export const forgotPasswordSchema = Yup.object({
-    email: Yup.string()
-        .email("Invalid email address")
-        .required("Email is required"),
-});
